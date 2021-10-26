809c414128c5661c7cfa20f334bd3c956ae76bff
tekayamehdi <mehdi.tekaya@esprit.tn>
2021-10-26 19:46:12 +0100

    composer config fix

--- lunettesMS/src/main/java/com/esprit/lunettes/LunettesMsApplication.java
+++ lunettesMS/src/main/java/com/esprit/lunettes/LunettesMsApplication.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class LunettesMsApplication {
 
 	public static void main(String[] args) {
