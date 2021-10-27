8bc7403ac4bc6e2b7e64806b4bcbf5b19204a138
medhabibhnini <60883388+medhabibhnini@users.noreply.github.com>
2021-10-27 21:37:52 +0100

    last before nodejs

--- zuul-server/src/main/java/com/esprit/gateway/communication/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/esprit/gateway/communication/ZuulServerApplication.java
@@ -2,6 +2,7 @@ package com.esprit.gateway.communication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import com.esprit.gateway.filters.*;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 @EnableFeignClients
 public class ZuulServerApplication {
